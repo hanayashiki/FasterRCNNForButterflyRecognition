c1a1b291a437d5d3ef6c52978fcd27598d62cd1f
494450105@qq.com <494450105@qq.com>
2018-05-13 13:52:14 +0800

    Rename address

--- butterfly_data_generator/config.py
+++ butterfly_data_generator/config.py
@@ -5,3 +5,5 @@ BASE_WILD_IMAGES_COMPRESSED = r"E:\butterfly-recognition\training-set-wild\train
 
 SOURCE_DATA_LIST = r"source\data_list.txt"
 SOURCE_DATA_LIST_COMPRESSED = r"source\data_list_compressed.txt"
+
+LINUX_BASE_WILD_IMAGES_COMPRESSED = r"/root/training-set-wild"
