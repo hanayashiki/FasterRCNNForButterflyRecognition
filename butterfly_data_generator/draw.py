70af314fd5f5b9ecbdbb3517422fbe54d7f90bec
494450105@qq.com <494450105@qq.com>
2018-05-13 23:01:35 +0800

    Set decay

--- butterfly_data_generator/draw.py
+++ butterfly_data_generator/draw.py
@@ -1,9 +1,9 @@
 import cv2
 
-def draw_rect(image, xmin, xmax, ymin, ymax):
+def draw_rect(image, xmin, ymin, xmax, ymax):
     return cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
 
-image = cv2.imread(r'E:\butterfly-recognition\training-set-wild\training-set\JPEGImagesCompressed\IMG_000425.jpg')
-image = draw_rect(image,349,491,211,347)
+image = cv2.imread(r'E:\butterfly-recognition\training-set-wild\training-set\JPEGImagesCompressed\IMG_000076.jpg')
+image = draw_rect(image,398,204,537,310)
 cv2.imshow('image',image)
 cv2.waitKey(0)
--- train_frcnn.py
+++ train_frcnn.py
@@ -150,8 +150,8 @@ else:
 	print('loading weights from {}'.format("model_frcnn.hdf5"))
 
 
-optimizer = Adam(lr=1e-5)
-optimizer_classifier = Adam(lr=1e-5)
+optimizer = Adam(lr=1e-5, decay=0.0005)
+optimizer_classifier = Adam(lr=1e-5, decay=0.0005)
 model_rpn.compile(optimizer=optimizer, loss=[losses.rpn_loss_cls(num_anchors), losses.rpn_loss_regr(num_anchors)])
 model_classifier.compile(optimizer=optimizer_classifier, loss=[losses.class_loss_cls, losses.class_loss_regr(len(classes_count)-1)], metrics={'dense_class_{}'.format(len(classes_count)): 'accuracy'})
 model_all.compile(optimizer='sgd', loss='mae')
